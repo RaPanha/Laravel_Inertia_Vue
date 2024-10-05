e5b84103a9544e467881197c085076327a65129b
rapanhalucky <rapanhalucky@gmail.com>
2024-10-05 09:26:35 +0700

    Setup Project

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        <p class="text-blue-500">This test pages</p>
+    </div>
+</template>
+
+<style></style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,30 @@
 import './bootstrap';
+
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+
+createInertiaApp({
+ resolve: (name) => {
+        return _resolvePageComponent(
+            name,
+            import.meta.glob("./Pages/**/*.vue")
+        );
+    },
+  setup({ el, App, props, plugin }) {
+    createApp({ render: () => h(App, props) })
+      .use(plugin)
+      .mount(el)
+  },
+})
+
+function _resolvePageComponent(name, pages) {
+    for (const path in pages) {
+        if (path.endsWith(`${name.replace(".", "/")}.vue`)) {
+            return typeof pages[path] === "function"
+                ? pages[path]()
+                : pages[path];
+        }
+    }
+
+    throw new Error(`Page not found: ${name}`);
+}
